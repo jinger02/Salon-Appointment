1a056945bfded9d6e4c79f327dd0c8802a910c15
jinger02 <maryjane.galindo@gmail.com>
2023-09-14 05:08:23 -0400

    add ask for appointment time

--- salon.sh
+++ salon.sh
@@ -15,7 +15,7 @@ MAIN(){
     SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
     echo "$SERVICES" | while read SERVICE_ID BAR SERVICE
     do
-      echo "$SERVICE_ID) $SERVICE_NAME"
+      echo "$SERVICE_ID) $SERVICE"
     done
 
     read SERVICE_ID_SELECTED
@@ -33,7 +33,32 @@ MAIN(){
 }
 
 APPOINT(){
-  
+    echo "What's your phone number? eg. 123-456-7890"
+    read PHONE_NUMBER
+    # check if phone number exist
+    if [[ ! $PHONE_NUMBER =~ ^[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9] ]]
+    then
+        echo -e "That is an invalid number.\n"
+        APPOINT
+    else
+        # check if in db
+        PHONE_NUMBER_RESULT=$($PSQL "SELECT phone FROM customers WHERE phone='$PHONE_NUMBER'")
+        # if does not exist
+        if [[ -z $PHONE_NUMBER_RESULT ]]
+        then
+            # ask for name
+            echo "I don't have a record for that phone number, what's your name?"
+            read CUSTOMER_NAME
+            # insert to database
+            NAME_PHONE_RESULT=$($PSQL "INSERT INTO customers(phone,name) VALUES('$PHONE_NUMBER','$CUSTOMER_NAME')")
+        fi
+        # ask for appointment time
+        CUSTOMER_NAME_RESULT=$($PSQL "SELECT name FROM customers WHERE phone='$PHONE_NUMBER'")
+        echo "What time would you like your cut,$CUSTOMER_NAME_RESULT?"
+
+
+    fi
 }
+
 
 MAIN
